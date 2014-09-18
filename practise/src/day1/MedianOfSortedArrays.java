275c98e83f4f2aa95ff9e288edd899afa7cc7a43
rsankar <rsankar@NBHYDRSANKAR-7.bedford.progress.com>
2014-09-18 10:56:01 +0530

    fixed MedianAlgo

--- practise/src/day1/MedianOfSortedArrays.java
+++ practise/src/day1/MedianOfSortedArrays.java
@@ -5,14 +5,82 @@ import java.util.Arrays;
 import model.TreeUtils;
 
 public class MedianOfSortedArrays {
-	private static int getMedianOfTwoSortedArrays(int a[], int b[]) {
+
+	private static double getMedianOfTwoSortedArrays(int[] a, int aStart,
+			int aEnd, int[] b, int bStart, int bEnd) {
+
+		int aMid = (aStart + aEnd) / 2;
+		int bMid = (bStart + bEnd) / 2;
+
+		if (aStart == aEnd) {
+			if ((bEnd - bStart + 1) % 2 == 0) {
+				return a[aStart] > b[bMid] ? b[bMid] : a[aStart];
+			} else {
+				return (double) (a[aStart] + b[bMid]) / 2;
+			}
+		}
+
+		if (aStart + 1 == aEnd) {
+			if (bStart + 1 == bEnd) {
+				int first = Math.max(a[aStart], b[bStart]);
+				int second = Math.min(a[aEnd], b[bEnd]);
+				return (first + second) / 2.0;
+			}
+
+			boolean aStartLess = a[aStart] < b[bMid];
+			boolean aEndGreater = a[aEnd] > b[bMid];
+
+			if ((bEnd - bStart + 1) % 2 == 0) {
+				if (aStartLess && aEndGreater) {
+					return (b[bMid] + Math.min(a[aEnd], b[bMid + 1])) / 2.0;
+				} else if (aStartLess && !aEndGreater) {
+					return (b[bMid] + Math.max(a[aEnd], b[bMid - 1])) / 2.0;
+				} else if (!aStartLess && aEndGreater) {
+					aStartLess = a[aStart] < b[bMid + 1];
+					aEndGreater = a[aEnd] > b[bMid + 1];
+					if (aStartLess && aEndGreater) {
+						return (a[aStart] + b[bMid + 1]) / 2.0;
+					} else if (aStartLess && !aEndGreater) {
+						return (a[aStart] + a[aEnd]) / 2.0;
+					} else if (!aStartLess && aEndGreater) {
+						return (a[aStart] + b[bMid + 1]) / 2.0;
+					} else {
+						return -1;
+					}
+				} else {
+					return -1;
+				}
+			} else {
+
+				if (aStartLess && aEndGreater) {
+					return b[bMid];
+				} else if (aStartLess && !aEndGreater) {
+					return Math.max(a[aEnd], b[bMid - 1]);
+				} else if (!aStartLess && aEndGreater) {
+					return Math.min(a[aStart], b[bMid + 1]);
+				} else {
+					return -1;
+				}
+			}
+		}
+		if (a[aMid] < b[bMid]) {
+			return getMedianOfTwoSortedArrays(a, aMid, aEnd, b, bStart, bEnd
+					- (aMid - aStart));
+		} else {
+			return getMedianOfTwoSortedArrays(a, aStart, aMid, b, bStart
+					+ (aEnd - aMid), bEnd);
+		}
+
+	}
+
+	private static int getMedianOfTwoSortedArraysNotWorking(int a[], int b[]) {
 		int leftPointer = 0;
 		int rightPointer = a.length - 1;
 		int median = (a.length + b.length) / 2;
 
 		while (leftPointer <= rightPointer) {
-			int leftPositionSortedIndexInOtherArray = TreeUtils.findSortedPositionIndex(
-					b, a[leftPointer]);
+			int leftPositionSortedIndexInOtherArray = TreeUtils
+					.findSortedPositionIndex(b, a[leftPointer]);
 			int leftPointerPositionInSortedArray = leftPositionSortedIndexInOtherArray
 					+ leftPointer;
 
@@ -25,8 +93,8 @@ public class MedianOfSortedArrays {
 						- (leftPointerPositionInSortedArray - median)];
 			}
 
-			int rightPositionSortedIndexInOtherArray = TreeUtils.findSortedPositionIndex(
-					b, a[rightPointer]);
+			int rightPositionSortedIndexInOtherArray = TreeUtils
+					.findSortedPositionIndex(b, a[rightPointer]);
 			int rightPointerPositionInSortedArray = rightPositionSortedIndexInOtherArray
 					+ rightPointer;
 
@@ -37,7 +105,7 @@ public class MedianOfSortedArrays {
 				return b[rightPositionSortedIndexInOtherArray - 1
 						+ (median - rightPointerPositionInSortedArray)];
 			}
-			
+
 			int origLeftPointer = leftPointer;
 
 			int tmp = rightPointer
@@ -47,12 +115,12 @@ public class MedianOfSortedArrays {
 			} else {
 				leftPointer++;
 			}
-			
-			tmp = origLeftPointer + (median-leftPointerPositionInSortedArray);
+
+			tmp = origLeftPointer + (median - leftPointerPositionInSortedArray);
 			if (tmp < rightPointer) {
 				rightPointer = tmp;
 			} else {
-				rightPointer --;
+				rightPointer--;
 			}
 			// leftPointer++;
 		}
@@ -61,16 +129,20 @@ public class MedianOfSortedArrays {
 
 	public static void main(String[] args) throws java.lang.Exception {
 
-		int a[] = TreeUtils.getRandomSortedArray(100,5000);
-		int b[] = TreeUtils.getRandomSortedArray(100,5000);
+		int a[] = TreeUtils.getRandomSortedArray(3500, 100000);
+		int b[] = TreeUtils.getRandomSortedArray(4050, 100000);
 
-		// a = new int[] { 37, 41, 43, 73, 99 };
-		// b = new int[] { 21, 23, 36, 39, 58 };
+//		System.out.println(Arrays.toString(a));
+//		System.out.println(Arrays.toString(b));
+//		a = new int[] { 287, 485, 656, 917 };
+//		b = new int[] { 53, 438, 680, 724, 908, 956 };
 		long startTime = System.nanoTime();
 		if (a.length < b.length) {
-			System.out.print(getMedianOfTwoSortedArrays(a, b));
+			System.out.print(getMedianOfTwoSortedArrays(a, 0, a.length - 1, b,
+					0, b.length - 1));
 		} else {
-			System.out.print(getMedianOfTwoSortedArrays(b, a));
+			System.out.print(getMedianOfTwoSortedArrays(b, 0, b.length - 1, a,
+					0, a.length - 1));
 		}
 		System.out.println(", " + (System.nanoTime() - startTime));
 
@@ -79,7 +151,13 @@ public class MedianOfSortedArrays {
 		System.arraycopy(a, 0, c, 0, a.length);
 		System.arraycopy(b, 0, c, a.length, b.length);
 		Arrays.sort(c);
-		System.out.println(c[c.length / 2] + ", "
-				+ (System.nanoTime() - startTime));
+		int length = c.length;
+		if (length % 2 == 1) {
+			System.out.println(c[length / 2] + ", "
+					+ (System.nanoTime() - startTime));
+		} else {
+			System.out.println((c[length / 2] + c[length / 2 - 1]) / 2.0 + ", "
+					+ (System.nanoTime() - startTime));
+		}
 	}
 }
